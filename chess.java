b368335d2a9eb336629e80d06412c999c6c98f19
NandiniSekar <61619403+NandiniSekar@users.noreply.github.com>
2024-11-12 19:27:34 +0530

    Update chess.java

--- chess.java
+++ chess.java
@@ -1,37 +1,36 @@
 
-
 import java.io.*;
 import java.util.*;
 
 
 class Solution {
   public static void main(String[] args) {
-   int bishoprow = 3;
-   int bishopcol = 5;
-   int piecerow = 5;
-   int piececol = 7;
-System.out.println(chessboaard(bishoprow,bishopcol,piecerow,piececol));
+ int br = 3;
+ int bc = 5;
+ int pr = 5;
+ int pc = 7;
+ System.out.println(chess(br,bc,pr,pc));
+}
 
+public static String chess(int br,int bc,int pr,int pc)
+{
+  if((br+bc)%2 != (pr+pc)%2)
+  {
+    return "NO"; //checking whether they are in same coloured squares
   }
 
-  public static String chessboaard(int bishoprow,int bishopcol,int piecerow,int piececol)
+  if((br==pr) && (bc==pc))
   {
-      if((bishoprow+bishopcol)%2 != (piececol+piecerow)%2)
-      {
-        return "NO";
-      }
-  if((bishoprow==piecerow) && (bishopcol==piececol))
-  {
-    return "Yes ,0";
+    return "YES";  //Both bishop & piece are in same position
   }
 
-  if((bishoprow-piecerow)==(bishopcol-piececol))
+  if(Math.abs(br-pr)== Math.abs(bc-pc))
   {
     return "Yes, 1";
   }
+  return "Yes, 2";
 
-    return "Yes, 2";
-  }
+}
 
 }
 
